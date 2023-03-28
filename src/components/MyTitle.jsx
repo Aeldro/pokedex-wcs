b61e97c5fc9b4d15681c8f62a183f5f9bffb6f5f
Nolan DE PUYDT <nodepuydt@gmail.com>
2023-03-28 09:56:51 +0200

    Added Bulbasaur

---
+++ src/components/MyTitle.jsx
@@ -0,0 +1,5 @@
+function MyTitle() {
+    return <h1>Hello React</h1>;
+  }
+  
+  export default MyTitle;
---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,8 @@
+function PokemonCard() {
+    return <figure>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+        <figcaption>bulbasaur</figcaption>
+    </figure>
+}
+
+export default PokemonCard;
