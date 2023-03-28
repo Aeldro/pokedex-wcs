ac0e2ae7c63684a053bffc3b6ce26aeec541705d
Nolan DE PUYDT <nodepuydt@gmail.com>
2023-03-28 11:16:15 +0200

    Added several pokemons and the way to switch to each other

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,47 @@ import PropTypes from "prop-types"
 
 const pokemonList = [
   {
-      name: "bulbasaur",
-      imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
-      name: "mew",
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
   },
 ];
 
 function App() {
+
+  const [pokemonIndex, setPokemonIndex] = useState(1)
+
+  const handlePreviousClick = () => {
+    setPokemonIndex(pokemonIndex - 1)
+  }
+
+  const handleNextClick = () => {
+    setPokemonIndex(pokemonIndex + 1)
+  }
+
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]} />
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      {pokemonIndex > 0 ? <button onClick={handlePreviousClick}>Précédent</button> : null}
+      {pokemonIndex < pokemonList.length - 1 ? <button onClick={handleNextClick}>Suivant</button> : null}
     </div>
   );
 }
